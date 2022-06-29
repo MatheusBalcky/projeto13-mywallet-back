9beb55e6bfd1925a2ba063f15491b8c6df34eec2
Matheus Balcky <matheuscabal821@gmail.com>
2022-06-29 12:58:29 -0300

    .post '/register' Saving user data with password encrypted

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'; dotenv.config();
 import Joi from 'joi';
+import bcrypt from 'bcrypt';
 import { MongoClient } from 'mongodb';
 
 
@@ -13,21 +14,25 @@ const client = new MongoClient(process.env.MONGO_URI);
 app.post('/register', async (req, res) =>{
     const userToRegister = req.body;
 
-
     try {
-        const { error } = await registerSchema.validateAsync(userToRegister);
+        const { error } = registerSchema.validate(userToRegister);
+        if(error){  throw new Error('Dados inválidos, preencha os campos corretamente')  };
+
         await client.connect(); console.log('Connected successfully to server');
         const db = client.db('myWallet'); const collection = db.collection('users');
-        const emailVerify = await collection.find({ email: req.body.email}).toArray();
-        if (emailVerify.length > 0){
-            throw new Error('Email já existente');
-        }
 
-        await collection.insertOne(userToRegister);
+        const emailVerify = await collection.find({ email: req.body.email}).toArray();
+        if (emailVerify.length > 0){  throw new Error('Email já existente') };
+
+        const passwordCrypted = bcrypt.hashSync(userToRegister.password,10)
+
+        await collection.insertOne({
+            ...userToRegister,
+            password: passwordCrypted
+        });
 
         res.status(200).send('Registrado no sistema com sucesso');
     } catch (error) { 
-        console.log(error);
 
         res.status(400).send(`${error}`);
     } finally {
@@ -40,7 +45,7 @@ app.post('/login', async (req, res) =>{
 
     try { 
         const { error } = await loginSchema.validateAsync(userLogin);
-
+        console.log(error)
         // ! APÓS VALIDAR O INPUT DO USUÁRIO COMO LOGIN, VERIFICAR SE ELE ESTÁ REGISTRADO NO BANCO DE DADOS...
         await client.connect(); console.log('Connected successfully to server');
         const db = client.db('myWallet'); const collection = db.collection('users');
@@ -76,8 +81,8 @@ const loginSchema = Joi.object({
 });
 
 const registerSchema = Joi.object({
-    name: Joi.string().pattern(/[a-zA-Z]{4,}/).required(),
-    email: Joi.string().pattern(/\S+@\S+\.\S+/).required(),
+    name: Joi.string().pattern(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/).required(),
+    email: Joi.string().email().required(),
     password: Joi.string().pattern(/.{6,21}/).required()
 })
 
